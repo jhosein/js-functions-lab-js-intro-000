c636336608bf5919b95a858dacbefb573686b1aa
Jonathan Hosein <jonathan.hosein@live.com>
2018-05-15 05:43:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ function happyHolidays(){
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`
 }
+
+function happyHolidayTo(holiday, name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountDown(holiday, days){
+  return `It's ${days} days until ${holiday}!`
+}
